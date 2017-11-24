7ebb8d8c46e5daa700036fe0143db9b69b055818
Ivashenyuk Yurii <yura.ivash@gmail.com>
2017-11-25 01:36:35 +0200

    added footer

--- index.php
+++ index.php
@@ -38,7 +38,11 @@
 	
 
 	<footer class="contentinfo">
-		
+		<div style="position: absolute; left: 15px; margin-top: 42px;">sport-store.localhost/ 10 з 10 на основі 20 оцінок. 18 клієнтських відгуків</div>
+		<div class="copyright">
+			&copy; 2017 Інтернет магазин спортивного обладнання. Всі права захищені
+		</div>
+
 	</footer>
 </body>
 </html>
