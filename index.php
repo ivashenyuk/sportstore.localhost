0ffc39d5e5311a23e4fb03bf8ce733d29f3699c4
Ivashenyuk Yurii <yura.ivash@gmail.com>
2017-11-25 01:13:06 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Sport store</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<header class="header">
+		<a href="/" class="logo"><img src="img/logo.png" alt="Sport-shop"></a>
+		<nav class="navigation">
+			<ul>
+				<li><a href="/index.php" class="gradient red border">Головна</a></li>
+				<li><a href="" class="gradient red border">Каталог</a></li>
+				<li><a href="" class="gradient red border">Таблиця розмірів</a></li>
+				<li><a href="" class="gradient red border">Акції</a></li>
+				<li><a href="" class="gradient red border">Контакти</a></li>
+				<li><a href="" class="gradient red border">Доставка</a></li>
+			</ul>
+		</nav>
+		<div class="contacts">
+				<span>Тел.: </span><span>(096) 493 26 47</span>
+				<span>Тел.: </span><span>(096) 493 26 47</span>
+		</div>
+		<div class="membership">
+			<a href="" class="ftn u">Вхід</a>
+			<a href="" class="ftn u">Реєстрація</a>
+		</div>
+		<div class="cart">
+			<div class="img empty"></div>
+			<div id="cart">
+				В корзині <span class="fnt s16 b">0</span> товарів <br>
+				на суму <span class="fnt s16 b">0</span> грн. <br>
+				<a href="" title="Оформити замовлення">Оформити замовлення</a>
+			</div>
+		</div>
+	</header>
+	
+
+	<footer class="contentinfo">
+		
+	</footer>
+</body>
+</html>
